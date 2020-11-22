50f96f20462d6d6c527cee577a30fd2ab12cd4c7
chiragrank <chiiragrank@gmail.com>
2020-11-22 02:50:46 -0500

    add demo.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+# author: Chirag Rank
+# date: 2020-11-21
+
+"""This script prints out docopt args.
+Usage: docopt.py <arg1> --arg2=<arg2> [--arg3=<arg3>]
+
+Options:
+<arg>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+"""
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
